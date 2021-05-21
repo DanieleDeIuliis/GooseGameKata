853f4aabb0bac4674f315b14d7d5af2ae8bfe348
DanieleDeIuliis <daniele.deiuliis@gmail.com>
2021-05-21 15:22:49 +0200

    refactoring

--- src/main/kotlin/InMemoryPlayer.kt
+++ src/main/kotlin/InMemoryPlayer.kt
@@ -1,19 +1,18 @@
 class InMemoryPlayer: PlayerRepository {
 
-    private val players = mutableListOf<String>()
-    private val players2 = mutableListOf<Player>()
+    private val players = mutableListOf<Player>()
 
     override fun add(playerName: String): Boolean {
-        if(players.contains(playerName)) {
+        if(players.any { it.name == playerName }) {
             return false
         }
-        players.add(playerName)
-        players2.add(Player(playerName, 0))
+
+        players.add(Player(playerName, 0))
         return true
     }
 
     override fun all(): List<String> {
-        return players
+        return players.map { it.name }
     }
 
     override fun updatePositionOf(player: String, position: Int) {
@@ -21,7 +20,7 @@ class InMemoryPlayer: PlayerRepository {
     }
 
     override fun positionOf(player: String): Int {
-        return players2.single { p -> p.name == player }.position
+        return players.single { p -> p.name == player }.position
     }
 
     data class Player(val name: String, val position: Int)
