84b0691c6bfc422cd24972dd73fef7db1216de99
DanieleDeIuliis <daniele.deiuliis@gmail.com>
2021-05-21 16:41:23 +0200

    extracted message build method in move command

--- src/main/kotlin/MoveCommand.kt
+++ src/main/kotlin/MoveCommand.kt
@@ -1,14 +1,24 @@
-class MoveCommand(private val repository: PlayerPositionRepository, private val commandData: MoveCommandData) : Command {
+class MoveCommand(private val repository: PlayerPositionRepository, private val commandData: MoveCommandData) :
+    Command {
     override fun exec(): String {
         commandData.apply {
             val startingPosition = repository.positionOf(playerName)
             val finalPosition = firstDiceRoll + secondDiceRoll + startingPosition
             repository.updatePositionOf(playerName, finalPosition)
 
-            val message = "$playerName rolls $firstDiceRoll, $secondDiceRoll. " +
+            return message(startingPosition, finalPosition)
+        }
+    }
+
+    private fun message(startingPosition: Int, finalPosition: Int): String {
+        commandData.apply {
+            var message = "$playerName rolls $firstDiceRoll, $secondDiceRoll. " +
                     "$playerName moves from ${startingPosition.toStartString()} to $finalPosition"
 
-            return if(finalPosition == 63) "$message. $playerName Wins!!" else message
+            if (finalPosition == 63) {
+                message += ". $playerName Wins!!"
+            }
+            return message
         }
     }
 
