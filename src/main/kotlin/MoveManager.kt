6e8dbea6940d84d3c6670085d0da2b43be30f8b7
DanieleDeIuliis <daniele.deiuliis@gmail.com>
2021-05-28 14:21:03 +0200

    extracted methods in move manager

--- src/main/kotlin/MoveManager.kt
+++ src/main/kotlin/MoveManager.kt
@@ -1,17 +1,32 @@
 class MoveManager(private val repository: PlayerPositionRepository, private val commandData: MoveCommandData) {
-     fun movePlayer(): Movement {
+    fun movePlayer(): Movement {
         val startingPosition = repository.positionOf(commandData.playerName)
-        var finalPosition = commandData.firstDiceRoll + commandData.secondDiceRoll + startingPosition
-        var bridgeTaken = false
-        if(finalPosition == 6) {
-            finalPosition = 12
-            bridgeTaken = true
-        }
-        var bouncedBack = 0
-        if(finalPosition > 63) {
-            bouncedBack = finalPosition - 63
-            finalPosition = 63 - bouncedBack
-        }
+        var positionAfterDiceRoll = commandData.firstDiceRoll + commandData.secondDiceRoll + startingPosition
+        val bridgeTaken = bridgeTaken(positionAfterDiceRoll)
+        val bouncedBack = bouncedBackAmount(positionAfterDiceRoll)
+        val finalPosition = computeFinalPositionFrom(positionAfterDiceRoll, bridgeTaken, bouncedBack)
         return Movement(startingPosition, finalPosition, bouncedBack, bridgeTaken)
+    }
+
+    private fun computeFinalPositionFrom(positionAfterDiceRoll: Int, bridgeTaken: Boolean, bouncedBack: Int): Int {
+        if (bridgeTaken) {
+            return 12
+        }
+        if (bouncedBack > 0) {
+            return 63 - bouncedBack
+        }
+        return positionAfterDiceRoll
+    }
+
+    private fun bridgeTaken(positionAfterDiceRoll: Int): Boolean {
+        return positionAfterDiceRoll == 6
+    }
+
+    private fun bouncedBackAmount(positionAfterDiceRoll: Int): Int {
+        var bouncedBack = 0
+        if (positionAfterDiceRoll > 63) {
+            bouncedBack = positionAfterDiceRoll - 63
+        }
+        return bouncedBack
     }
 }
